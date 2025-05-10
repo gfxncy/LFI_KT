a8d3113af749f0690cee6b16d81f4398a7985a4d
gfxncy <31383720+gfxncy@users.noreply.github.com>
2025-05-10 22:44:56 +0300

    class Simulation method init_input_file

--- miniproject/test_auto.cpp
+++ miniproject/test_auto.cpp
@@ -2,11 +2,17 @@
 
 int main() {
     std::string output_file_path;
+    std::string electrode_positions_line;
+    std::string electrode_widths_line;
+    std::string electrode_voltages_line;
  
     std::ifstream simulation_input_file("/Users/alekseidushanin/Documents/GitHub/LFI_KT/miniproject/dataset/sim_input.txt");
     if (simulation_input_file.is_open())
     {
         std::getline(simulation_input_file, output_file_path);
+        std::getline(simulation_input_file, electrode_positions_line);
+        std::getline(simulation_input_file, electrode_widths_line);
+        std::getline(simulation_input_file, electrode_voltages_line);
         std::ofstream output_file(output_file_path);
         output_file << "Hello world!";
     }
